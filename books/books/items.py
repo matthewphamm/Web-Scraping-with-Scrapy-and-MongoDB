e2d3c3f42bd53283abd870a19a402fa8d312ddee
Matthew Pham <matthewpham03@gmail.com>
2026-07-09 20:07:07 -0700

    Remove dataclass

--- books/books/items.py
+++ books/books/items.py
@@ -3,10 +3,8 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-from dataclasses import dataclass
 import scrapy
 
-@dataclass
 class BooksItem(scrapy.Item):
     _id = scrapy.Field()
     url = scrapy.Field()
