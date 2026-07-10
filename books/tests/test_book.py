07aaea49c0bdc46546e0eef855f68e78f0177480
Matthew Pham <matthewpham03@gmail.com>
2026-07-09 20:39:39 -0700

    Add unit tests

---
+++ books/tests/test_book.py
@@ -0,0 +1,18 @@
+import unittest
+
+class BookSpiderTest(unittest.TestCase):
+
+    def test_parse_scrapes_all_items(self):
+        """Test if the spider scrapes books and pagination links."""
+        pass
+
+    def test_parse_scrapes_correct_book_information(self):
+        """Test if the spider scrapes the correct information for each book."""
+        pass
+
+    def test_parse_creates_pagination_request(self):
+        """Test if the spider creates a pagination request correctly."""
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
