55490f290d41ac3e1f762fb40a7b5ebd1f1245fd
Matthew Pham <matthewpham03@gmail.com>
2026-07-09 20:45:08 -0700

    Add setUp method for each test

--- books/tests/test_book.py
+++ books/tests/test_book.py
@@ -1,6 +1,18 @@
 import unittest
+from scrapy.http import HtmlResponse
+from books.spiders.book import BookSpider
 
 class BookSpiderTest(unittest.TestCase):
+    def setUp(self):
+        self.spider = BookSpider()
+        self.example_html = """
+            Insert the example HTML here
+        """
+        self.response = HtmlResponse(
+            url="https://books.toscrape.com",
+            body=self.example_html,
+            encoding="utf-8"
+        )
 
     def test_parse_scrapes_all_items(self):
         """Test if the spider scrapes books and pagination links."""
